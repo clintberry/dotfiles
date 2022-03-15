5191627d43069d337ce357df7ecea0c9bc5fec39
CB <clint@getkolla.com>
2022-03-15 13:56:08 -0600

    Added github setup instructions

--- scripts/github.sh
+++ scripts/github.sh
@@ -1,6 +1,16 @@
+echo "Generate and add ssh key"
 read -p "Enter github email : " email
 echo "Using email $email"
 echo "Generating key"
 ssh-keygen -t rsa -b 4096 -C "$email"
 ssh-add ~/.ssh/id_rsa
-gh ssh-key add ~/.ssh/id_rsa.pub
+gh auth login # This auto adds your ssh key as part of its process
+
+echo "Generate GPG key"
+gpg --full-generate-key
+
+
+# see https://docs.github.com/en/authentication/managing-commit-signature-verification/generating-a-new-gpg-key
+# gpg --armor --export [[ID goes here]]
+
+
