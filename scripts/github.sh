0b0685aa54be907cc1d6a368664a53f8995981e6
CB <clint@getkolla.com>
2022-04-19 16:05:57 -0600

    Added code as default editor for git

--- scripts/github.sh
+++ scripts/github.sh
@@ -2,9 +2,11 @@ echo "Set git default settings"
 read -p "Enter github email : " email
 # Set default push behavior to match branch name automatically
 git config --global push.default current
+git config --global init.defaultBranch main
 # Set default git email
 git config --global user.email $email
 git config --global user.name "Clint Berry"
+git config --global core.editor "code --wait"
 echo "Generate and add ssh key"
 echo "Using email $email"
 echo "Generating key"
