391e8985a47c2796a3878df9e9978fb8d90e0d66
Clint Berry <clint.b.berry@gmail.com>
2024-03-07 23:44:25 -0700

    test

--- go.sh
+++ go.sh
@@ -57,8 +57,8 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/the
 ln -s $DOTFILES/.p10k.zsh $HOME/.p10k.zsh
 
 # Symlink the Mackup config file to the home directory
-ln -s ./.mackup.cfg $HOME/.mackup.cfg
-mackup restore
+ln -s $DOTFILES/.mackup.cfg $HOME/.mackup.cfg
+mackup restore -f
 
 
 ##### VSCode stuff
@@ -78,6 +78,33 @@ code --install-extension GitHub.codespaces
 code --install-extension GitHub.copilot
 code --install-extension GitHub.copilot-chat
 code --install-extension mikestead.dotenv
+
+
+echo "Set git default settings"
+export email=clint.b.berry@gmail.com
+# Set default push behavior to match branch name automatically
+git config --global push.default current
+git config --global init.defaultBranch main
+# Set default git email
+git config --global user.email $email
+git config --global user.name "Clint Berry"
+git config --global core.editor "code --wait"
+
+echo "Generating a new SSH key for GitHub..."
+
+# Generating a new SSH key
+# https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
+ssh-keygen -t ed25519 -C $email -f ~/.ssh/id_ed25519
+
+# Adding your SSH key to the ssh-agent
+# https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
+eval "$(ssh-agent -s)"
+
+touch ~/.ssh/config
+echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
+
+ssh-add -K ~/.ssh/id_ed25519
+gh auth login # This auto adds your ssh key as part of its process
 
 
 ##### MacOS settings - run last
